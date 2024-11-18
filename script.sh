355fd4d1f5495999511102dd14903dfdcc84c750
Marcelo Martiniano <marcelo@webjasper.com.br>
2024-11-18 00:42:00 -0300

    Ajuste no shell

--- script.sh
+++ script.sh
@@ -1,75 +1,90 @@
 #!/bin/bash
 
-iniciar(){
+iniciar() {
     echo
-    echo "Selecione uma opção"
+    echo "Selecione uma opção:"
     echo
     echo "1 - Instalar programas"
     echo "2 - Sair"
     echo
 }
 
-apt_install(){
+apt_install() {
     echo
-    echo "atualizando repositórios: "
+    echo "Atualizando repositórios..."
     echo
     sudo apt update &&
+
     echo
-    echo "instalanco nodejs " 
+    echo "Instalando Node.js..."
     echo
     sudo apt install nodejs -y &&
-    echo 
-    echo "instalando npm "
+
+    echo
+    echo "Instalando NPM..."
     echo
     sudo apt install npm -y &&
-    echo 
-    echo "instalando postgresql "
+
+    echo
+    echo "Instalando PostgreSQL..."
     echo
     sudo apt install postgresql postgresql-contrib -y &&
+
     echo
-    echo "instalando pm2" 
+    echo "Instalando PM2..."
     echo
     sudo npm install pm2@latest -g &&
+
     echo
-    echo "Instalando redis: "
+    echo "Instalando Redis..."
     echo
-    sudo sudo apt install redis -y &&
+    sudo apt install redis -y &&
+
     echo
-    echo "instalando git"
+    echo "Instalando Git..."
     echo
-    sudo apt install git &&
-    echo 
-    echo "Instalando bun "
+    sudo apt install git -y &&
+
+    echo
+    echo "Instalando Bun..."
     echo
     sudo npm install -g bun &&
+
     echo
-    echo "copiando o arquivo pg_hba.conf"
+    echo "Clonando o repositório do GIT..."
     echo
- #   sudo scp /home/henderson/pg_hba.conf univates@177.44.248.60:/etc/postgresql/16/main/pg_hba.conf &&
+    if [ -d "f1_api" ]; then
+        echo "O repositório já foi clonado."
+    else
+        sudo git clone https://github.com/fronhaa/f1_api
+    fi
+
     echo
-    echo "iniciando o PM2, e start do app.ts"
+    echo "Iniciando o PM2 e rodando app.ts..."
     echo
-    sudo pm2 start ts-node app.ts &&
+    cd f1_api || exit
+    sudo pm2 start ts-node app.ts
+
     echo
-    echo "Clonando o repositório do GIT:"
-    echo 
-    sudo git clone https://github.com/fronhaa/f1_api
+    echo "Instalação concluída!"
     echo
-    
-    iniciar  
 }
 
 iniciar
-while read -p "Qual a opção que você deseja: " entrada
-do
 
-    if [ $entrada -eq 1 ]; then
-        echo "Iniciando: "
-        apt_install
-        break
-    elif [ $entrada -eq 2 ]; then
-        echo "see you! "
-        exit
+while read -p "Qual a opção que você deseja: " entrada; do
+    if [[ "$entrada" =~ ^[0-9]+$ ]]; then
+        if [ "$entrada" -eq 1 ]; then
+            echo "Iniciando instalação..."
+            apt_install
+            break
+        elif [ "$entrada" -eq 2 ]; then
+            echo "Saindo..."
+            exit
+        else
+            echo "Opção inválida. Tente novamente."
+        fi
+    else
+        echo "Por favor, digite um número válido."
     fi
 done
-
